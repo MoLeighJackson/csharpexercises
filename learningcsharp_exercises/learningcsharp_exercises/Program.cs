1088d8e301fbe7e7ae248790fb41718f817c2ee5
Mo Leigh <moleighjackson@gmail.com>
2017-04-16 04:57:19 -0400

    Add files via upload

---
+++ learningcsharp_exercises/learningcsharp_exercises/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningcsharp_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int counter = 0; counter < 11; counter++)  //intialize counter to 0, conditional - when the counter is less than 10, increment by 1
+                {
+                Console.WriteLine(counter);
+                }
+           
+
+        }
+    }
+}
