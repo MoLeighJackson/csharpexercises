09332eab507ec00bd3f592844d22752091cb1a76
MoLeighJackson <moleighjackson@gmail.com>
2017-04-21 00:01:03 -0400

    understanding c# class files
    walkthrough of creating class in visual studio

---
+++ learningcsharp_exercises/learningcsharp_exercises/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningcsharp_exercises
+{
+    class Pet
+    {
+       //member date types
+        private string color;
+        private string breed;
+        private int age;
+        private int yearAdopted;
+
+        //function
+        public void walk()
+        {
+            Console.WriteLine("Walked outside");
+        }
+
+        public void speak()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+}
